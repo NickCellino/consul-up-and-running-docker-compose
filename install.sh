ecdb331142afcd1e2fd069eb783bc8d876c93770
Nick Cellino <nick.cellino@hashicorp.com>
2023-02-11 16:02:41 -0500

    have FE and BE communicating

--- install.sh
+++ install.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
 mkdir -p /opt/consul
-echo "Starting Consul..."
-consul agent -config-dir=/config/
+#echo "Starting Consul..."
+#consul agent -config-dir=/config/
+
+if [ $SERVICE == "frontend" ]; then
+  echo "Starting frontend"
+  ./bin/frontend
+fi
+
+if [ $SERVICE == "backend" ]; then
+  echo "Starting backend"
+  ./bin/backend
+fi
 
